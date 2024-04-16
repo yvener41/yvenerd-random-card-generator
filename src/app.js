36476a4795594669a7f4d9ec869991f2457fde69
Yvener Dutervil <72567854+yvener41@users.noreply.github.com>
2024-04-16 02:35:35 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = ["♦", "♥", "♠", "♣"];
+  let value = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  function generateRandomSuit() {
+    return suit[Math.floor(Math.random() * suit.length)];
+  }
+
+  function generateRandomValue() {
+    return value[Math.floor(Math.random() * value.length)];
+  }
+
+  let randomSuit = generateRandomValue();
+  let randomValue = generateRandomSuit();
+
+  document.querySelector(".card").innerHTML =
+    randomValue + randomSuit + randomValue;
+
+  let cardValue = document.querySelector(".card");
+  cardValue.innerHTML = `${randomValue} ${randomSuit} ${randomValue}`;
+
+  if (randomSuit == "♥" || randomSuit == "♠") {
+    cardValue.style.color = "red";
+  }
+
+  console.log(`${randomValue} ${randomSuit} ${randomValue}`);
 };
